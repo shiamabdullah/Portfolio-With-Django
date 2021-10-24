4863da7b4280fa9a75d6a5a02a98c0fce4ab3f22
shiam <shiamabdullah1@gmail.com>
2021-10-25 04:46:04 +0600

    heroku

--- shiamPortfolio/shiamPortfolio/settings.py
+++ shiamPortfolio/shiamPortfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4d)ksq!a=808vpq40ci2nk73uybf54ipgwpw@l+$g$bw+_%nps'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000/', 'shiam-django-portfolio.herokuapp.com/']
 
 
 # Application definition
