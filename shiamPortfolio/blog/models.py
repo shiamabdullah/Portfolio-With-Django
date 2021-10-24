dc90e34bc4cf332e322b4723ea0f4c50a1281b71
shiam <shiamabdullah1@gmail.com>
2021-10-25 02:17:55 +0600

    blogs

--- shiamPortfolio/blog/models.py
+++ shiamPortfolio/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=40)
+    description = models.CharField(max_length=200)
+    dateCreated = models.DateField(auto_now_add=True)
+    image= models.ImageField(upload_to='portfolio/images/')
+    url= models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.title
